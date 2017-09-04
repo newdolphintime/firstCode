885e11f5875a7037266db0b0f430e44756ce5f2c
zhangwei <1551583950@qq.com>
2017-09-04 23:20:48 +0800

    第一行代码学习笔记

--- app/src/main/java/com/paly/zv/firstcode/MainActivity.java
+++ app/src/main/java/com/paly/zv/firstcode/MainActivity.java
@@ -1,31 +1,69 @@
 package com.paly.zv.firstcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //任何资源都会在R文件中生成一个相应资源ID
         setContentView(R.layout.activity_main);
         /*
         * LOG级别由低到高
         *
         * */
-        Log.v("verbose","打印最为琐碎的，意义最小的日志信息");
+        Log.v("verbose", "打印最为琐碎的，意义最小的日志信息");
 
-        Log.d("debug","打印一些调试信息");
+        Log.d("debug", "打印一些调试信息");
 
-        Log.i("info","打印一些比较重要的数据");
+        Log.i("info", "打印一些比较重要的数据");
 
-        Log.w("warn","打印一些警报信息");
+        Log.w("warn", "打印一些警报信息");
 
-        Log.e("error","错误信息日志");
+        Log.e("error", "错误信息日志");
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //显式调用intent 显式意图开启另一个活动
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"you click add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"you click remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
     //输入logt按下Tab键会以当前类名自动生成一个TAG常量
     private static final String TAG = "MainActivity";
 }
