79464ce06587c7263d069fe8cac38c2e19fbafbd
zhangwei <1551583950@qq.com>
2017-09-04 21:05:58 +0800

    第一行代码学习笔记

--- app/src/main/java/com/paly/zv/firstcode/MainActivity.java
+++ app/src/main/java/com/paly/zv/firstcode/MainActivity.java
@@ -10,8 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
+        * LOG级别由低到高
+        *
+        * */
         Log.v("verbose","打印最为琐碎的，意义最小的日志信息");
 
+        Log.d("debug","打印一些调试信息");
+
+        Log.i("info","打印一些比较重要的数据");
+
+        Log.w("warn","打印一些警报信息");
+
+        Log.e("error","错误信息日志");
+
+
     }
+    //输入logt按下Tab键会以当前类名自动生成一个TAG常量
+    private static final String TAG = "MainActivity";
 }
