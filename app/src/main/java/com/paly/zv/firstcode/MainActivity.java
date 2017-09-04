9f3056d9d213838061b7aa0c35a4fbad66cfd8af
zhangwei <1551583950@qq.com>
2017-09-04 20:50:13 +0800

    第一行代码学习笔记

--- app/src/main/java/com/paly/zv/firstcode/MainActivity.java
+++ app/src/main/java/com/paly/zv/firstcode/MainActivity.java
@@ -2,6 +2,7 @@ package com.paly.zv.firstcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("verbose","打印最为琐碎的，意义最小的日志信息");
+
     }
 }
